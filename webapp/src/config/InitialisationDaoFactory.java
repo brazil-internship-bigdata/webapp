e237a2dbb641e28444b255b09edef067d08b2deb
geourjoa <anthony.geourjon@gmail.com>
2017-06-17 00:09:15 -0300

    Update ClassPath, Target Runtime. Update DaoConfigurationException. Add InitialisationDaoFactory

---
+++ webapp/src/config/InitialisationDaoFactory.java
@@ -0,0 +1,27 @@
+package config;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import dao.DAOFactory;
+
+public class InitialisationDaoFactory implements ServletContextListener {
+	private static final String	ATT_DAO_FACTORY	= "daofactory";
+
+	private DAOFactory			daoFactory;
+
+	@Override
+	public void contextInitialized(ServletContextEvent event) {
+
+		ServletContext servletContext = event.getServletContext();
+
+		this.daoFactory = DAOFactory.getInstance();
+		// The Dao is now globale
+		servletContext.setAttribute(ATT_DAO_FACTORY, this.daoFactory);
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent event) {
+	}
+}
