fdf865039a8c2e080b7b57735edf26b6646ce4cf
geourjoa <anthony.geourjon@gmail.com>
2017-06-17 01:19:39 -0300

    Servlet for restriction and exception

---
+++ webapp/src/forms/FormValidationException.java
@@ -0,0 +1,11 @@
+package forms;
+
+@SuppressWarnings("serial")
+public class FormValidationException extends Exception {
+	/*
+	 * Constructeur
+	 */
+	public FormValidationException(String message) {
+		super(message);
+	}
+}
